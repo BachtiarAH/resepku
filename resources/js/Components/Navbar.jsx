2f48c3bf33976f58c87f57c30f6795a576972912
BachtiarAH <bachtiarah73@gmail.com>
2023-07-06 23:08:17 +0700

    [add] logout

--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar(params) {
                 <NavLink href={'/dashboard'}><h1 className="h1 flex text-white">Resepku</h1></NavLink>
                 <ul className="mt-auto mb-auto">
                     <NavLink href={'/recipe/create'}>Tulis Resep</NavLink>
-                    <NavLink>Logout</NavLink>
+                    <NavLink href={route('logout')} method="post" as="button">Logout</NavLink>
                 </ul>
             </div>
         </nav>
